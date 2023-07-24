0f002c999e840d80ef6fd56ae0c603216370e278
senaalkaya <sena.alkaya@btyon.com>
2023-07-24 10:56:41 +0300

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Ej2Grid from './pages/ej2'
-
+import  Muı from './pages/mui'
 function App() {
 
   return (
     <>
-      <Ej2Grid></Ej2Grid>
+      {/* <Ej2Grid></Ej2Grid> */}
+      <Muı></Muı>
     </>
   )
 }
